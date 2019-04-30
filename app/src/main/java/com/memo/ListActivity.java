a03b332919961fa368a104098ade2dfa3831cf61
user <>
2019-04-30 12:27:50 +0900

    無駄な変数

--- app/src/main/java/com/memo/ListActivity.java
+++ app/src/main/java/com/memo/ListActivity.java
@@ -19,6 +19,8 @@ public class ListActivity extends AppCompatActivity {
     //検索HITフラグ
     public static ArrayList<String> hit = new ArrayList<>();
 
+    int muda = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
